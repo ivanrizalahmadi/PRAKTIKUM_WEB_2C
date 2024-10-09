7b1597ac19406d9e5c8b7a2fdb28a101635bfcd7
IvanRizal <ivanrizal36@gmail.com>
2024-10-09 14:28:28 +0700

    praktikum 5

---
+++ JS7_FormProcces/regex.php
@@ -0,0 +1,32 @@
+<?php
+// Cocokkan huruf kecil 'a'
+$pattern = '/[a]/'; 
+$text = 'This is a Sample Text.';
+if (preg_match($pattern, $text)) {
+    echo "Huruf kecil ditemukan!<br>";
+} else {
+    echo "Tidak ada huruf kecil!<br>";
+}
+
+// Cocokkan satu atau lebih digit
+$pattern = '/[0-9]+/';
+$text = 'There are 123 apples.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0] . "<br>";
+} else {
+    echo "Tidak ada yang cocok!<br>";
+}
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie.';
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text; // Output: "I like banana pie."
+
+$pattern = '/go{1,3}d/';
+$text = 'god is good, gooood is too much.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0] . "<br>";
+} else {
+    echo "Tidak ada yang cocok!<br>";
+}
+?>
