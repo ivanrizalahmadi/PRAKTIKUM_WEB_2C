709516259965cd7da8ff9dce9829114d2da5d3bf
IvanRizal <ivanrizal36@gmail.com>
2024-10-16 13:30:49 +0700

    Praktikum ke 2

---
+++ JS8_FormUpload/proses_upload.php
@@ -0,0 +1,36 @@
+<?php
+
+$targetDirectory = "images/";
+
+// Periksa apakah direktori penyimpanan ada, jika tidak maka buat
+if (!file_exists($targetDirectory)) {
+    mkdir($targetDirectory, 0777, true);
+}
+
+$allowedExtensions = array("jpg", "jpeg", "png", "gif");
+
+if ($_FILES['files']['name'][0]) {
+    $totalFiles = count($_FILES['files']['name']);
+
+    // Loop melalui semua file yang diunggah
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $fileName = $_FILES['files']['name'][$i];
+        $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+        $targetFile = $targetDirectory . $fileName;
+
+        // Periksa apakah file memiliki ekstensi gambar yang valid
+        if (in_array($fileType, $allowedExtensions)) {
+            // Pindahkan file yang diunggah ke direktori penyimpanan
+            if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+                echo "File $fileName berhasil diunggah.<br>";
+            } else {
+                echo "Gagal mengunggah file $fileName.<br>";
+            }
+        } else {
+            echo "File $fileName tidak valid. Hanya file gambar yang diperbolehkan.<br>";
+        }
+    }
+} else {
+    echo "Tidak ada file yang diunggah.";
+}
+?>
