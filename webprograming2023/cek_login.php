0553a0f86b003b08f939873f810ca0093d8a6387
IvanRizal <ivanrizal36@gmail.com>
2024-11-06 15:40:40 +0700

    Langkah ke 3 dan 4

--- webprograming2023/cek_login.php
+++ webprograming2023/cek_login.php
@@ -7,16 +7,14 @@ include "config/koneksi.php";
 include "fungsi/pesan_kilat.php";
 include "fungsi/anti_injection.php";
 
-// Mengambil data username dan password dari form
 $username = antiinjection($koneksi, $_POST['username']);
 $password = antiinjection($koneksi, $_POST['password']);
 
 $query = "SELECT username, level, salt, password AS hashed_password FROM user WHERE username = '$username'";
 $result = mysqli_query($koneksi, $query);
 
-// Cek jika query gagal
 if (!$result) {
-    die("Query gagal: " . mysqli_error($koneksi));
+    die("Query gagal: " . mysqli_error($koneksi)); // Cek error query
 }
 
 $row = mysqli_fetch_assoc($result);
@@ -26,35 +24,19 @@ if ($row) {
     $salt = $row['salt'];
     $hashed_password = $row['hashed_password'];
 
-    // Verifikasi password
-    if ($hashed_password !== null) {
-        // Jika salt kosong, hanya verifikasi password
-        if ($salt === null || $salt === '') {
-            if (password_verify($password, $hashed_password)) {
-                // Set session jika login berhasil
-                $_SESSION['username'] = $row['username'];
-                $_SESSION['level'] = $row['level'];
-                header("Location: index.php");
-                exit();
-            } else {
-                pesan('danger', "Login gagal. Password Anda Salah.");
-                header("Location: login.php");
-                exit();
-            }
+    if ($salt !== null && $hashed_password !== null) {
+        $combined_password = $salt . $password;
+
+        if (password_verify($combined_password, $hashed_password)) {
+            // Set session jika login berhasil
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['level'] = $row['level'];
+            header("Location: index.php");
+            exit();
         } else {
-            // Jika salt ada, kombinasikan password dengan salt
-            $combined_password = $salt . $password;
-            if (password_verify($combined_password, $hashed_password)) {
-                // Set session jika login berhasil
-                $_SESSION['username'] = $row['username'];
-                $_SESSION['level'] = $row['level'];
-                header("Location: index.php");
-                exit();
-            } else {
-                pesan('danger', "Login gagal. Password Anda Salah.");
-                header("Location: login.php");
-                exit();
-            }
+            pesan('danger', "Login gagal. Password Anda Salah.");
+            header("Location: login.php");
+            exit();
         }
     }
 } else {
